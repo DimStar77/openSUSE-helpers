d1f772a9a6e6a0b13bc35a05ef9423a839310d3b
Dominique Leuenberger <dimstar@opensuse.org>
2015-03-17 23:27:59 +0100

    appdata.html:  add script to keep daily diffs available

---
+++ appstream-trend/update-appdata_html.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pushd $(dirname $0)
+
+APPDATA="http://download.opensuse.org/tumbleweed/repo/oss/suse/setup/descr/appdata.html"
+
+curl $APPDATA > appdata.html
+git commit appdata.html -m "appdata.html: $(date +%Y-%m-%d)"
