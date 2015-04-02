c02eb8432e1e9012c3ea839075682b325351b139
Dominique Leuenberger <dimstar@opensuse.org>
2015-04-02 19:53:11 +0200

    rebuild: check repository for errors and attempt to fix with appropriate rebuilds

---
+++ osc-plugins/CreatePackageDescr.pm
@@ -0,0 +1,165 @@
+package CreatePackageDescr;
+
+BEGIN {
+    unshift @INC, "/usr/lib/build/Build";
+}
+
+use File::Basename;
+
+use Rpm;
+use Fcntl qw/:flock/;
+
+sub package_snippet($) {
+
+    my $package = shift;
+
+    my $cachedir  = dirname($package) . "/.cache/";
+    my $cachefile = $cachedir . basename($package);
+
+    my $out = '';
+    if ( -f $cachefile ) {
+        open( C, '<', $cachefile ) || die "no cache for $package";
+        flock(C, LOCK_SH) or die "failed to lock $cachefile: $!\n";
+        while (<C>) {
+            $out .= $_;
+        }
+        close(C);
+        return $out;
+    }
+
+    # RPMTAG_FILEMODES            = 1030, /* h[] */
+    # RPMTAG_FILEFLAGS            = 1037, /* i[] */
+    # RPMTAG_FILEUSERNAME         = 1039, /* s[] */
+    # RPMTAG_FILEGROUPNAME        = 1040, /* s[] */
+
+    my %qq = Build::Rpm::rpmq(
+        $package,
+        qw{NAME VERSION RELEASE ARCH OLDFILENAMES DIRNAMES BASENAMES DIRINDEXES 1030 1037 1039 1040
+          1047 1112 1113 1049 1048 1050 1090 1114 1115 1054 1053 1055 1036 5046 5047 5048 5049 5050 5051
+          5052 5053 5054 5055 5056 5057 1156 1158 1157 1159 1161 1160
+          }
+    );
+
+    my $name = $qq{'NAME'}[0];
+
+    Build::Rpm::add_flagsvers( \%qq, 1049, 1048, 1050 );    # requires
+    Build::Rpm::add_flagsvers( \%qq, 1047, 1112, 1113 );    # provides
+    Build::Rpm::add_flagsvers( \%qq, 1090, 1114, 1115 );    # obsoletes
+    Build::Rpm::add_flagsvers( \%qq, 1054, 1053, 1055 );    # conflicts
+
+   Build::Rpm::add_flagsvers(\%qq, 1156, 1158, 1157) if $qq{1156}; # oldsuggests
+   Build::Rpm::add_flagsvers(\%qq, 1159, 1161, 1160) if $qq{1159}; # oldenhances
+
+   Build::Rpm::add_flagsvers(\%qq, 5046, 5048, 5047) if $qq{5046}; # recommends
+   Build::Rpm::add_flagsvers(\%qq, 5049, 5051, 5050) if $qq{5049}; # suggests
+   Build::Rpm::add_flagsvers(\%qq, 5052, 5054, 5053) if $qq{5052}; # supplements
+   Build::Rpm::add_flagsvers(\%qq, 5055, 5057, 5056) if $qq{5055}; # enhances
+
+    $out .= sprintf( "=Pkg: %s %s %s %s\n",
+        $name, $qq{'VERSION'}[0], $qq{'RELEASE'}[0], $qq{'ARCH'}[0] );
+    $out .= "+Flx:\n";
+    my @modes      = @{ $qq{1030}       || [] };
+    my @basenames  = @{ $qq{BASENAMES}  || [] };
+    my @dirs       = @{ $qq{DIRNAMES}   || [] };
+    my @dirindexes = @{ $qq{DIRINDEXES} || [] };
+    my @users      = @{ $qq{1039}       || [] };
+    my @groups     = @{ $qq{1040}       || [] };
+    my @flags      = @{ $qq{1037}       || [] };
+    my @linktos    = @{ $qq{1036}       || [] };
+
+    my @xprvs;
+
+    foreach my $bname (@basenames) {
+        my $mode   = shift @modes;
+        my $di     = shift @dirindexes;
+        my $user   = shift @users;
+        my $group  = shift @groups;
+        my $flag   = shift @flags;
+        my $linkto = shift @linktos;
+
+        my $filename = $dirs[$di] . $bname;
+        my $fs       = $filename;
+        if ( Fcntl::S_ISLNK($mode) ) {
+            $fs = "$filename -> $linkto";
+        }
+        $out .= sprintf "%o %o %s:%s %s\n", $mode, $flag, $user, $group, $fs;
+        if (   $filename =~ /^\/etc\//
+            || $filename =~ /bin\//
+            || $filename eq "/usr/lib/sendmail" )
+        {
+            push @xprvs, $filename;
+        }
+
+    }
+    $out .= "-Flx:\n";
+    $out .= "+Prv:\n";
+    foreach my $prv ( @{ $qq{1047} || [] } ) {
+        $out .= "$prv\n";
+    }
+    foreach my $prv (@xprvs) {
+        $out .= "$prv\n";
+    }
+    $out .= "-Prv:\n";
+    $out .= "+Con:\n";
+    foreach my $prv ( @{ $qq{1054} || [] } ) {
+        $out .= "$prv\n";
+    }
+    $out .= "-Con:\n";
+    $out .= "+Req:\n";
+    foreach my $prv ( @{ $qq{1049} || [] } ) {
+        next if ( $prv =~ m/^rpmlib/ );
+        next
+          if ( $name eq "libqmmp0-plugin-mplayer"
+            && $prv eq "/usr/bin/mplayer" );
+        next if ( $prv eq "this-is-only-for-build-envs" );
+        next
+          if ( $name eq "installation-images-debuginfodeps"
+            && $prv =~ m/debuginfo.build/ );
+        next
+          if ( $name eq "installation-images-debuginfodeps-openSUSE"
+            && $prv =~ m/debuginfo.build/ );
+        $out .= "$prv\n";
+    }
+    $out .= "-Req:\n";
+
+    $out .= "+Obs:\n";
+    foreach my $prv ( @{ $qq{1090} || [] } ) {
+        $out .= "$prv\n";
+    }
+    $out .= "-Obs:\n";
+
+    $out .= "+Rec:\n";
+    foreach my $prv ( @{ $qq{5046} || [] } ) {
+        $out .= "$prv\n";
+    }
+    $out .= "-Rec:\n";
+
+    $out .= "+Sup:\n";
+    foreach my $prv ( @{ $qq{5052} || [] } ) {
+        $out .= "$prv\n";
+    }
+    $out .= "-Sup:\n";
+
+    $out .= "+Enh:\n";
+    foreach my $prv ( @{ $qq{5055} || [] } ) {
+        $out .= "$prv\n";
+    }
+    $out .= "-Enh:\n";
+
+    $out .= "+Sug:\n";
+    foreach my $prv ( @{ $qq{5049} || [] } ) {
+        $out .= "$prv\n";
+    }
+    $out .= "-Sug:\n";
+
+    mkdir($cachedir);
+    open( C, '>>', $cachefile ) || die "can't open $cachefile";
+    flock(C, LOCK_EX) or die "failed to lock $cachefile: $!\n";
+    seek(C, 0, 0); truncate(C, 0);
+    print C $out;
+    close(C);
+
+    return $out;
+}
+
+1;
---
+++ osc-plugins/rebuildpacs.pl
@@ -0,0 +1,351 @@
+#! /usr/bin/perl
+
+use Data::Dumper;
+use XML::Simple;
+use URI::Escape;
+use File::Basename;
+use File::Temp qw/tempdir/;
+use Digest::MD5 qw(md5_hex);
+
+my $script_dir;
+
+BEGIN {
+    ($script_dir) = $0 =~ m-(.*)/-;
+    $script_dir ||= '.';
+    unshift @INC, $script_dir;
+}
+
+require CreatePackageDescr;
+
+my @repodirs;
+
+sub find_source_container($) {
+    my $pkg = shift;
+
+    for my $repodir (@repodirs) {
+      my @rpms = glob("$repodir/*-$pkg.rpm");
+      for my $rpm (@rpms) {
+        # 1123 == Disturl
+        my %qq = Build::Rpm::rpmq( $rpm, qw{NAME 1123} );
+        next if ( $qq{NAME}[0] ne $pkg );
+        my $distfile = basename( $qq{1123}[0] );
+        $distfile =~ s,^[^-]*-,,;
+
+        return $distfile;
+      }
+    }
+}
+
+my $followup = 0;
+my $cproblem = '';
+my %problems;
+
+my $project = $ARGV[0] || "openSUSE:Factory";
+my $repo    = $ARGV[1] || "standard";
+my $arch    = $ARGV[2] || "x86_64";
+
+my %leafed;
+
+sub read_plain_index($) {
+  my $file = shift;
+
+  my %ret;
+
+  open(FILE, $file) || return \%ret;
+  while ( <FILE> ) {
+    if (m/^(.*):(.*)/) {
+      $ret{$1} = $2;
+    }
+  }
+  close(FILE);
+  return \%ret;
+}
+
+sub write_plain_index($$) {
+  my $file = shift;
+  my $hash = shift;
+
+  open(FILE, ">$file") || die "can't write to $file";
+  for my $key (sort keys %{$hash}) {
+    print FILE "$key:" . $hash->{$key} . "\n";
+  }
+  close(FILE);
+}
+
+# defines packages that need to be triggered too
+my %parents = (
+    "rpmlint"           => [qw(rpmlint-mini)],
+    "branding-openSUSE" => [
+        qw(glib2-branding-openSUSE
+          kiwi-config-openSUSE
+          xfce4-branding-openSUSE
+          kdebase4-openSUSE kde-branding-openSUSE
+          bundle-lang-kde installation-images-openSUSE)
+      ],
+    "kdebase4-openSUSE" => [qw(bundle-lang-kde)],
+  );
+# for subsets (staging projects) we need to remember which are ignored
+my %ignored;
+
+sub check_leaf_package($$) {
+
+    my $package = shift;
+    my $rebuildhash = shift;
+
+    if (system("osc api /source/$project/$package/_meta > /dev/null 2>&1 ")) {
+	$ignored{$package} = 1;
+	return;
+    }
+
+    my @lines = ();
+    open( OSC, "osc api /build/$project/$repo/$arch/$package/_buildinfo?internal=1|" );
+    while (<OSC>) {
+        chomp;
+        if (m/<subpack>(.*)</) {
+            $leafed{$1} = $package;
+        }
+        if (m/bdep name="([^"]*)"/) {
+            my $parent = $leafed{$1};
+            if ( $parent && $parent ne "rpmlint-mini" ) {
+	      # I dislike grep
+	      unless (grep { $_ eq $package } @{$parents{$parent}}) {
+		print "ADD $package to PARENT $parent!!\n";
+	      }
+	      next;
+            }
+        }
+        else {
+            next;
+        }
+        next if (m/notmeta="1"/);
+        push( @lines, $_ );
+    }
+    close(OSC);
+    my $ctx = Digest::MD5->new;
+    for my $line ( sort @lines ) {
+      $ctx->add($line);
+    }
+    my $rebuilds = read_plain_index("buildinfos");
+    my $newmd5 = $ctx->hexdigest;
+    if ($rebuilds->{"$project/$repo/$arch/$package"} ne $newmd5) {
+
+      $rebuildhash->{$package} = 1;
+      for my $child (@{$parents{$package}}) {
+	$rebuildhash->{$child} = 1;
+      }
+      $rebuilds->{"$project/$repo/$arch/$package"} = $newmd5;
+      write_plain_index("buildinfos", $rebuilds);
+    }
+}
+
+my %torebuild;
+check_leaf_package("rpmlint", \%torebuild);
+check_leaf_package("rpmlint-mini", \%torebuild);
+
+check_leaf_package("branding-openSUSE", \%torebuild);
+check_leaf_package("glib2-branding-openSUSE", \%torebuild);
+check_leaf_package("PackageKit-branding-openSUSE", \%torebuild);
+check_leaf_package("kiwi-config-openSUSE", \%torebuild);
+check_leaf_package("xfce4-branding-openSUSE", \%torebuild);
+check_leaf_package("kdebase4-openSUSE", \%torebuild);
+check_leaf_package("kde-branding-openSUSE", \%torebuild);
+
+check_leaf_package("bundle-lang-common", \%torebuild);
+check_leaf_package("bundle-lang-kde", \%torebuild);
+check_leaf_package("bundle-lang-gnome", \%torebuild);
+check_leaf_package("installation-images-openSUSE", \%torebuild);
+if (%torebuild) {
+  my $api = "/build/$project?cmd=rebuild&repository=$repo&arch=$arch";
+  for my $package (sort keys %torebuild) {
+    next if (defined $ignored{$package});
+    $api .= "&package=" . uri_escape( $package );
+  }
+  system("osc api -X POST '$api'");
+}
+
+my $pfile = tempdir() . "/packages";    # the filename is important ;(
+
+sub mirror_repo($$$) {
+
+  my $project = shift;
+  my $repo = shift;
+  my $arch = shift;
+
+  my $repodir = ($ENV{XDG_CACHE_HOME}||$ENV{HOME}."/.cache")."/opensuse-repo-checker/repo-$project-$repo-$arch";
+  mkdir($repodir);
+
+  system(
+    "$script_dir/bs_mirrorfull --nodebug https://build.opensuse.org/build/$project/$repo/$arch/ $repodir"
+  );
+  return $repodir;
+}
+
+sub find_package_in_project($) {
+  my $project = shift;
+
+  open(OSC, "osc api /source/$project?expand=1 |");
+  my $xml = XMLin(join('', <OSC>), ForceArray => 1);
+  close(OSC);
+  my @packs = keys %{$xml->{entry}};
+  return shift @packs;
+}
+
+
+# find a random package
+
+
+sub get_paths($$$) {
+  my $project = shift;
+  my $repo = shift;
+  my $arch = shift;
+
+  my $package = find_package_in_project($project);
+
+  open(OSC, "osc api /build/$project/$repo/$arch/$package/_buildinfo|");
+  my $xml = join('', <OSC>);
+  if ($xml !~ m/^</) {
+     die "failed to open /build/$project/$repo/$arch/$package/_buildinfo"; 
+  }
+  $xml = XMLin($xml, ForceArray => 1);
+  close(OSC);
+
+  return $xml->{path};
+}
+
+my $paths = get_paths($project, $repo, $arch);
+my @rpms;
+
+for my $path (@$paths) {
+
+  # openSUSE:Factory/ports is in the paths, but not a repo
+  if (system("osc api /build/$path->{'project'}/$path->{'repository'}/$arch > /dev/null 2>&1 ")) {
+    next;
+  }
+
+  my $repodir = mirror_repo($path->{'project'}, $path->{'repository'}, $arch);
+  push(@repodirs, $repodir);
+  push(@rpms, glob("$repodir/*.rpm"));
+}
+
+open( PACKAGES, ">", $pfile ) || die "can not open $pfile";
+print PACKAGES "=Ver: 2.0\n";
+
+my %knipser;
+
+foreach my $package (@rpms) {
+    die $package unless $package =~ m,/.{32}-([^/]+)\.rpm$,;
+    next if $knipser{$1}++;
+    my $out = CreatePackageDescr::package_snippet($package);
+    print PACKAGES $out;
+}
+close(PACKAGES);
+
+# read the problems out of installcheck
+open( INSTALLCHECK, "installcheck $arch $pfile|" );
+while (<INSTALLCHECK>) {
+    chomp;
+
+    if (m/^can't install (.*)\-[^-]*\-[^-]*\.($arch|noarch):/) {
+        $cproblem = $1;
+        $cproblem =~ s/kmp-([^-]*)/kmp-default/;
+        $cproblem = find_source_container($cproblem);
+        $followup = 0;
+        next;
+    }
+
+    $followup = 1 if ( $_ =~ m/none of the providers can be installed/ );
+
+    # not interesting for me
+    next if (m/  \(we have /);
+    next if ($followup);
+
+    # very thin ice here
+    s,\(\)\(64bit\),,;
+
+    s,(needed by [^ ]*)\-[^-]*\-[^-]*\.($arch|noarch)$,$1,;
+
+    s,^\s*,,;
+    # patterns are too spammy and rebuilding doesn't help
+    next if (grep { $_ eq $cproblem } qw(patterns-openSUSE));
+    $problems{$cproblem}->{$_} = 1;
+
+}
+close(INSTALLCHECK);
+unlink($pfile);
+rmdir(dirname($pfile));
+
+for my $package ( sort keys %problems ) {
+    $problems{$package} = join( ', ', sort( keys %{ $problems{$package} } ) );
+}
+
+my @other_problems;
+my %oproblems;
+
+open( PROBLEMS, "problems" );
+while (<PROBLEMS>) {
+    chomp;
+    if (m,^$project/$repo/$arch/([^:]*):\s*(.*)$,) {
+        my $package = $1;
+        my $oproblem = $2;
+        # remember old problems for current project/repo
+        $oproblems{$package} = $oproblem;
+    }
+    else {
+        # keep all lines for other projects/repos as they are
+        push @other_problems, $_;
+    }
+}
+close(PROBLEMS);
+
+exit(0) if ( !%problems );
+
+# check for succeeded packages - we can't filter as we don't know if the problems are all in the top project ;(
+my $api = "/build/$project/_result?repository=$repo&arch=$arch&code=succeeded";
+
+open( RESULT, "osc api '$api'|" );
+@result = <RESULT>;
+my $results = XMLin( join( '', @result ), ForceArray => ['status'] );
+close(RESULT);
+
+my @packages = @{ $results->{result}->{status} };
+my $rebuildit = 0;
+
+$api = "/build/$project?cmd=rebuild&repository=$repo&arch=$arch";
+for my $package (@packages) {
+    $package = $package->{package};
+
+    if (!$problems{$package}) {
+      # it can go
+      delete $oproblems{$package};
+      next;
+    }
+
+    my $oproblem = $oproblems{$package} || '';
+    if ($problems{$package} eq $oproblem) {
+	# rebuild won't help
+	next;
+    }
+    $rebuildit = 1;
+    print "rebuild ", $package, ": ",
+      $problems{ $package }, "\n";
+    $api .= "&package=" . uri_escape( $package );
+    $oproblems{$package} = $problems{$package};
+}
+
+open( PROBLEMS, ">problems" );
+# write all lines for other projects/repos as they are
+foreach (@other_problems) {
+    print PROBLEMS $_, "\n";
+}
+for my $package (keys %oproblems) {
+    print PROBLEMS "$project/$repo/$arch/"
+      . $package . ": "
+      . $oproblems{ $package }, "\n";
+}
+close(PROBLEMS);
+
+if ($rebuildit) {
+  print "API '$api'\n";
+  system("osc api -X POST '$api'");
+}
+
