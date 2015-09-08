c1fc0b3c59e670b1d808adb4d079f4edd221323e
Dominique Leuenberer <dimstar@opensuse.org>
2015-09-08 11:05:20 +0000

    uppdate-appdata: we now publish the file in .xz format

--- appstream-trend/update-appdata_html.sh
+++ appstream-trend/update-appdata_html.sh
@@ -2,7 +2,9 @@
 
 pushd $(dirname $0)
 
-APPDATA="http://download.opensuse.org/tumbleweed/repo/oss/suse/setup/descr/appdata.html"
+APPDATA="http://download.opensuse.org/tumbleweed/repo/oss/suse/setup/descr/appdata.html.xz"
 
-curl $APPDATA > appdata.html
+rm appdata.html appdata.html.xz
+curl $APPDATA > appdata.html.xz
+unxz appdata.html.xz
 git commit appdata.html data.txt -m "appdata.html: $1"
