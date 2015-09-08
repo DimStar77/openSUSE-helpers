9c819f87ebe649f5096218ac7caaf690b9829efd
Dominique Leuenberer <dimstar@opensuse.org>
2015-09-08 11:30:07 +0000

    update-appdata_html: do not redownload the xz files.. we already prepared them

--- appstream-trend/update-appdata_html.sh
+++ appstream-trend/update-appdata_html.sh
@@ -2,9 +2,11 @@
 
 pushd $(dirname $0)
 
-APPDATA="http://download.opensuse.org/tumbleweed/repo/oss/suse/setup/descr/appdata.html.xz"
+#APPDATA="http://download.opensuse.org/tumbleweed/repo/oss/suse/setup/descr/appdata.html.xz"
 
-rm appdata.html appdata.html.xz
-curl $APPDATA > appdata.html.xz
-unxz appdata.html.xz
+#rm appdata.html appdata.html.xz
+#curl $APPDATA > appdata.html.xz
+#unxz appdata.html.xza
+
+# We already downloaded the .xz file earlier and decompressed it
 git commit appdata.html data.txt -m "appdata.html: $1"
