c792cc3cac4a70cfd7e1526142aa0aed5687e910
Dominique Leuenberger <dimstar@opensuse.org>
2016-10-12 14:22:25 +0200

    Parse repo/package/repo/arch from one string
    This allows to copy the 'last part' of a build log url and just
    throw it at the script

--- Tumbleweed-Dev-Scripts/trigger-if-failed.sh
+++ Tumbleweed-Dev-Scripts/trigger-if-failed.sh
@@ -1,10 +1,28 @@
+old_IFS=$IFS
+IFS=/ read PRJ PKG REPO ARCH dump <<< "$1"
+IFS=$old_IFS
 
-PRJ=$1
-PKG=$2
-REPO=$3
-ARCH=$4
-DELAY=$5
+if [ -n "$PRJ" -a -n "$PKG" -a -n "$REPO" -a -n "$ARCH" ]; then
+  DELAY=$2
+else
+  PRJ=$1
+  PKG=$2
+  REPO=$3
+  ARCH=$4
+  DELAY=$5
+fi
 retry=0
+
+if [ -z "$PRJ" -o -z "$PKG" -o -z "$REPO" -o -z "$ARCH" ]; then
+  echo " USAGE"
+  echo " ====="
+  echo "$0 needs to know what project/package/repo/arch to test/trigger"
+  echo "Call it as $0 <PRJ> <PKG> <REPO> <ARCH> [<DELAY>]"
+  echo "or alternatively (to make copy/paste easier)"
+  echo "$0 <PRG>/<PKG>/<REPO>/<ARCH> [<DELAY>]"
+  echo "The 2nd form allows to copy/paste part of OBS' build log URL"
+  exit 1
+fi
 
 if [ -z "$DELAY" ]; then
 	DELAY=60
@@ -23,7 +41,7 @@ while (sleep $DELAY); do
 			retry=$[$retry + 1]
 			;;	
 		"succeeded")
-			echo "Package $PRJ/$PKG successfully built for $REPO/$ARCJ after $retry tries"
+			echo "Package $PRJ/$PKG successfully built for $REPO/$ARCH after $retry tries"
 			break
 			;;
 	esac
