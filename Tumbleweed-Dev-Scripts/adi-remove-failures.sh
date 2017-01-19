ba93ff85f93410c90d887cea8505ef2ebb413840
Dominique Leuenberger <dimstar@opensuse.org>
2017-01-19 13:59:32 +0100

    Add adi-remove-failure script
    Remove all packages from an adi staging project that are currently failing
    (failed or unresolvable). This allows the remaining packages in the staging
    project to move through the process without further waiting.

---
+++ Tumbleweed-Dev-Scripts/adi-remove-failures.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Scans an adi Staging and unselects everything currently failing or unresolvable, leaving only 'successful' packages
+# in the staging. This can help for adi that are completely reviewed, but blocked by some failing package
+# The failed ones will move to a random new adi staging
+
+if [ -z "$1" ]; then
+	echo "Need to be called as $0 <n> - with <n> referencing the number of the adi project to process"
+	exit
+fi
+
+osc prjresults openSUSE:Factory:Staging:adi:$1 -V | awk '/^[FU] / {print $2}' | xargs -r osc staging unselect
