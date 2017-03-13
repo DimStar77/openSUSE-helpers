08bc636c40afadcee8d0f1b3f95df3380c9b584b
Dominique Leuenberger <dimstar@opensuse.org>
2017-03-13 22:48:16 +0100

    rebuild-dep-chain.sh: Add progress bar

--- Tumbleweed-Dev-Scripts/rebuild-dep-chain.sh
+++ Tumbleweed-Dev-Scripts/rebuild-dep-chain.sh
@@ -17,8 +17,9 @@ if dialog --title "Trigger build of ${NUM_TRIGGER} packages?" --yesno "The packa
 	echo "You selected to trigger the whole package list... starting."
 	i=1
 	for PKG in $PKGLIST; do
-		echo -n "[$i/${NUM_TRIGGER}] - $PKG: "
-		osc rebuildpac openSUSE:Factory $PKG -r standard
+		PCT=$[100 * $i / ${NUM_TRIGGER}]
+		echo XXX| dialog --gauge "Trigger $i / ${NUM_TRIGGER} packages for rebuild ($PCT%)\nCurrent package: $PKG" 7 70 $PCT
+		osc rebuildpac openSUSE:Factory $PKG -r standard > /dev/null
 		i=$(($i + 1))
 	done
 else
