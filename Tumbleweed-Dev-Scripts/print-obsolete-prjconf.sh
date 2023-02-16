6038f4dab23b8fb7f66a337bdde517bc4f1d4d73
Dominique Leuenberger <dimstar@opensuse.org>
2023-02-16 15:23:40 +0100

    print-obsolete-prjconf: add support to identify obsolete onlybuild entries
    Those entries will become relevant when TW disables i586 builds for most things
    And we want to have a chance to keep the list clean

--- Tumbleweed-Dev-Scripts/print-obsolete-prjconf.sh
+++ Tumbleweed-Dev-Scripts/print-obsolete-prjconf.sh
@@ -9,11 +9,28 @@ TMPDIR=$(mktemp -d)
 
 for PORT in "" :ARM :PowerPC :zSystems; do osc ls -b openSUSE:Factory${PORT} -r standard; done | sed 's/\.rpm$//' | sort -u > ${TMPDIR}/all_packages
 
+echo "### Prefer: packages to verify - the package names listed here do not exist"
+
 for pkg in $(for word in $(osc meta prjconf openSUSE:Factory | \
     grep Prefer: | \
     sed "s/Prefer://g"); do echo $word; done | \
     sed "s/^-//g" | \
-    sed 's/.*://g' | 
-    \sort -u); do 
+    sed 's/.*://g' | \
+    sort -u); do 
       grep -q "\\s${pkg}$" ${TMPDIR}/all_packages || echo "$pkg";
      done
+
+# Checking onlybuild entries
+for prj in openSUSE:Factory openSUSE:Factory:NonFree; do
+  osc prjresults --show-excluded ${prj} -a x86_64 -r standard -V | awk '/^[.FUf%bsSx]/ {print $2}' >> ${TMPDIR}/all_sources
+done
+
+echo using ${TMPDIR}/all_packages
+
+echo
+echo "### onlybuild|excludebuid: packages to verify - the package names listed here do not exist"
+echo
+
+for pkg in $(osc meta prjconf openSUSE:Factory | grep "BuildFlags" | sed 's/BuildFlags: onlybuild://'); do
+      grep -q "^${pkg}$" ${TMPDIR}/all_sources || echo "Drop only|exclude build for $pkg";
+  done
