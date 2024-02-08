167b3a70f977405325031efa4c34c5dba7b840fc
Dominique Leuenberger <dimstar@opensuse.org>
2024-02-08 10:50:08 +0100

    Clean up attic

--- Attic/Tumbleweed-Dev-Scripts/gcd_live_size.sh
+++
@@ -1,9 +0,0 @@
-
-IMG=$1
-
-if [ -z "$IMG" ]; then
-  echo "No paramter specified, assuming gnome"
-  IMG=gnome
-fi
-
-osc rbl openSUSE:Factory:Live kiwi-image-livecd-$IMG standard x86_64 | awk -F= '/\+ size=/ {print $2}'
