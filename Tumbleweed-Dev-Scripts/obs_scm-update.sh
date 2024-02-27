c04131017a3002fbe50c80fdb16914b66a9364eb
Dominique Leuenberger <dimstar@opensuse.org>
2024-02-27 13:49:03 +0100

    obs_scm_update.sh: aid in updating packages using obs_scm source service
    Can be started without parameter, then it takes @PARENT_TAG@ as revision (i.e latest tag)
    alternatively, call it with a tag name, a revision, a branch name (e.g main) as parameter.
    
    Creates diffs of NEWS and meson.* between the latest packaged version and the new one

---
+++ Tumbleweed-Dev-Scripts/obs_scm-update.sh
@@ -0,0 +1,40 @@
+
+
+if [ ! -d .osc ]; then
+  echo "This seems not to be an osc checkout - aborting"
+  exit 1
+fi
+
+if [ -z "$1" ]; then
+  REV="@PARENT_TAG@"
+else
+  REV="$1"
+fi
+
+if [ ! -f _service ]; then
+  echo "Package has not been converted to a _service managed package"
+  exit 3
+fi
+
+OLD_REV=$(awk '/commit:/ {print $2}' *.obsinfo 2>/dev/null)
+PKGNAME=$(grep '<param name="url">.*</param>' _service | sed -e 's/<.*">//' -e's/<.*>//' -e 's/ //g' -e 's/\.git$//' | awk -F/ '{print $NF}')
+
+rm *.obscpio 2> /dev/null
+
+sed -i "s|<param name=\"revision\">.*</param>|<param name=\"revision\">${REV}</param>|" _service
+osc service mr
+
+VERSION=$(awk '/version:/ {print $2}' *.obsinfo)
+
+if [ ! -z "$OLD_REV" ]; then
+  pushd $PKGNAME
+  git diff $OLD_REV..HEAD -- NEWS > ../osc-collab.NEWS
+  git diff $OLD_REV..HEAD -- meson.build > ../osc-collab.meson
+  git diff $OLD_REV..HEAD -- meson_options.txt > ../osc-collab.meson_options
+popd
+fi
+
+osc vc -m "Update to version $VERSION:"
+
+osc ar
+
