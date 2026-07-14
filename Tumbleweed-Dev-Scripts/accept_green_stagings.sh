bc43b34722e1c560bfdde92aec2c5c18f3398b1f
Dominique Leuenberger <dimstar@opensuse.org>
2026-07-14 09:48:24 +0200

    Integrate devel_update to configure for new packages / devel project entries

--- Tumbleweed-Dev-Scripts/accept_green_stagings.sh
+++ Tumbleweed-Dev-Scripts/accept_green_stagings.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if ! type -p devel_update.sh 2>/dev/null; then
+  echo '*****************************************'
+  echo 'devel_update.sh script not found in $PATH'
+  echo.
+  echo 'We cannot update the devel_packages file for'
+  echo 'new packages, which results in those packages'
+  echo 'not being imported to git'
+  echo '*****************************************'
+  echo 'For now, this is a warning only'
+fi
+
 if ! osc staging lock -m "Factory accept bot"; then
    echo "Cannot aquire lock - please try again later"
    exit 1
@@ -24,27 +35,23 @@ if [ "$stdver" -ne "$openqa" ]; then
     exit 1
 fi
 
+if type -p devel_update.sh 2>/dev/null; then
+  MKTEMP=$(mktemp -d)
+  pushd $MKTEMP
+    git clone gitea@src.opensuse.org:openSUSE/Factory.git
+    pushd Factory/pkgs/_meta
+      devel_update.sh syncnewpackages
+      git commit devel_packages -m "Update devel project for new packages on $today"
+      git push
+    popd
+  popd
+  rm -rf "$MKTEMP"
+fi
+
 osc staging --wipe-cache list --supersede
 osc staging --wipe-cache list --supersede --project openSUSE:Factory:NonFree
 osc staging adi
 osc staging adi --project openSUSE:Factory:NonFree
-
-# echo Finding acceptable staging projects
-
-# ACCPRJS now including adi stagings
-# ACCPRJS=$(/usr/bin/osc api /staging/openSUSE:Factory/staging_projects?status=1 | egrep 'staging_project name="openSUSE:Factory:Staging:.*" state="acceptable"' | egrep -o 'name="openSUSE:Factory:Staging:[^"]+' | sed 's/name="openSUSE:Factory:Staging://')
-
-#for prj in $ACCPRJS; do
-#  ACCPRJ="$ACCPRJ $prj"
-#done
-#
-#if [ -z "$ACCPRJ" ]; then
-#  echo "No staging project to accept - skipping non-ring-only accept run"
-#  osc staging unlock
-#  exit 1
-#fi
-
-# echo "Acceptable projects${ACCPRJ}"
 
 # First accept NonFree; after accepting OSS, there is a chance that NonFree stagings already start building again
 osc staging accept --project openSUSE:Factory:NonFree
